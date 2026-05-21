51c233994c6515b086b3341ba5c39db61ad5c4fb
KOJ-hashi <s.nitkagoshima@gmail.com>
2026-05-21 23:05:19 +0900

    Add search functionality for frequency data

--- app.js
+++ app.js
@@ -1,14 +1,109 @@
+const data=[
+
+{
+
+name:"鹿児島TWR01",
+
+freq:"118.2",
+
+mode:"AM",
+
+tags:"鹿児島 KOJ RJFK"
+
+},
+
+{
+
+name:"鹿児島APP01",
+
+freq:"126.0",
+
+mode:"AM",
+
+tags:"鹿児島 KOJ RJFK"
+
+},
+
+{
+
+name:"羽田TWR01",
+
+freq:"118.1",
+
+mode:"AM",
+
+tags:"羽田 HND RJTT"
+
+}
+
+];
+
+
 function searchFreq(){
 
 let word=
-document.getElementById(
+
+document
+.getElementById(
 "search"
-).value;
+)
+.value
+.toUpperCase();
 
-document.getElementById(
+let result="";
+
+data.forEach(d=>{
+
+if(
+
+d.tags
+.toUpperCase()
+.includes(word)
+
+||
+
+d.name
+.toUpperCase()
+.includes(word)
+
+){
+
+result +=
+
+`
+<div>
+
+<h3>${d.name}</h3>
+
+<p>
+
+${d.freq}
+
+${d.mode}
+
+</p>
+
+</div>
+
+<hr>
+
+`;
+
+}
+
+});
+
+if(result===""){
+
+result="見つかりません";
+
+}
+
+document
+.getElementById(
 "result"
-).innerHTML=
+)
 
-"検索："+word;
+.innerHTML=result;
 
 }
