8272bd8885801dd57bf8edaa40f85b6561c762ef
KOJ-hashi <s.nitkagoshima@gmail.com>
2026-05-21 23:02:12 +0900

    Remove CSV fetching and streamline searchFreq function
    Refactor data fetching and search functionality.

--- app.js
+++ app.js
@@ -1,97 +1,14 @@
-let data=[];
-
-Promise.all([
-
-fetch("data/KOJ.csv")
-.then(r=>r.text()),
-
-fetch("data/HND.csv")
-.then(r=>r.text())
-
-])
-
-.then(files=>{
-
-files.forEach(csv=>{
-
-let rows=csv.split("\n");
-
-rows.shift();
-
-rows.forEach(row=>{
-
-let c=row.split(",");
-
-data.push({
-
-name:c[0],
-
-freq:c[1],
-
-category:c[2],
-
-subcategory:c[3],
-
-mode:c[4],
-
-region:c[5],
-
-tags:c[6]
-
-});
-
-});
-
-});
-
-});
-
-
 function searchFreq(){
 
 let word=
-document
-.getElementById(
+document.getElementById(
 "search"
 ).value;
 
-let html="";
-
-data.forEach(d=>{
-
-if(
-
-d.tags.includes(word)
-
-||
-
-d.name.includes(word)
-
-){
-
-html+=`
-
-<hr>
-
-${d.name}
-
-<br>
-
-${d.freq}
-
-${d.mode}
-
-`;
-
-}
-
-});
-
-document
-.getElementById(
+document.getElementById(
 "result"
-)
+).innerHTML=
 
-.innerHTML=html;
+"検索："+word;
 
 }
